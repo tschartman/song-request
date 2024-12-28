2fa176f0088881400dc3b5859e9099121df1b676
tim <tschartman@yahoo.com>
2024-12-28 10:15:52 -0600

    optimize for mobile

--- src/app/SongsRequestList.js
+++ src/app/SongsRequestList.js
@@ -3,12 +3,12 @@ export default function SongRequestList({ songRequests }) {
 
   return (
     <div className="w-full mt-4">
-      <h3 className="text-xl font-medium text-gray-800 mb-2">Requested Songs</h3>
+      <h3 className="text-lg font-medium text-gray-800 mb-2 sm:text-xl">Requested Songs</h3>
       <ul className="w-full border border-gray-300 rounded-md shadow-md bg-white">
         {songRequests.map((song) => (
           <li
             key={song.id}
-            className="py-2 px-4 border-b last:border-none flex justify-between items-center"
+            className="py-2 px-4 border-b last:border-none flex justify-between items-center text-sm sm:text-base"
           >
             <span>
               <strong>{song.title}</strong> by {song.artist}
